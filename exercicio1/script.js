a82dcff631b35306ec572842c8756c3a3739565e
vitorfontenele <vitor_fontenele@hotmail.com>
2022-08-30 13:01:30 -0300

    Exercício 1 resolvido

---
+++ exercicio1/script.js
@@ -0,0 +1,10 @@
+//Pede ao user que insira um numero par
+const numeroPar = prompt("Insira um número par");
+numeroPar = Number(numeroPar);
+
+//Imprime o resto da divisão desse numero por 2
+console.log(numeroPar % 2);
+
+//Para números pares, o valor do resto da divisão impresso sempre será zero.
+
+//Para números ímpares, o resto da divisão será sempre 1.
