91021b5f78024f7e977b51512c8a256ce6e62676
vitorfontenele <vitor_fontenele@hotmail.com>
2022-08-30 13:18:34 -0300

    Exercício 3 resolvido

---
+++ exercicio3/script.js
@@ -0,0 +1,11 @@
+//Operacoes logicas
+const operacao1 = (5 > 20) && (5 < 2);
+const operacao2 = (5 == 5) || (5 == '5');
+const operacao3 = !(20 > 50);
+const operacao4 = !((20 > 50) || (50 > 60));
+
+//Imprimindo operacoes
+console.log(operacao1);
+console.log(operacao2);
+console.log(operacao3);
+console.log(operacao4);
